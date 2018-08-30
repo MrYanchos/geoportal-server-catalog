b272ef419aadf51d55a1fe0975156ea60a4eb2b1
David Valentine <david.valentine@gmail.com>
2018-08-30 11:45:17 -0700

    Searchbox typeahead  multiple terms in the search box

--- geoportal/src/main/webapp/custom/searchBoxTypeahead.js
+++ geoportal/src/main/webapp/custom/searchBoxTypeahead.js
@@ -1,66 +1,66 @@
+var dictUrl= "http://ec-scigraph.sdsc.edu:9000/scigraph/vocabulary/autocomplete/%QUERY?limit=20&searchSynonyms=true&searchAbbreviations=false&searchAcronyms=false&includeDeprecated=false";
+var params = {"query": "%QUERY","limit":"20" };
+var keyCol = "";
 
-            var dictUrl = "http://ec-scigraph.sdsc.edu:9000/scigraph/vocabulary/autocomplete/%QUERY?limit=20&searchSynonyms=false&searchAbbreviations=false&searchAcronyms=false&includeDeprecated=false";
-            var params = {"query": "%QUERY", "limit": "20"};
-      // print( query(".typeahead-text") );
-            var taCol = new Bloodhound({
-                datumTokenizer: function (datum) {
-                    return Bloodhound.tokenizers.whitespace(datum.value);
-                },
-                queryTokenizer: Bloodhound.tokenizers.whitespace,
-                remote: {
-                    wildcard: params.query,
-                    url: dictUrl,
-                    prepare: function (query, settings) {
-                        gPre = query;
-                        taPick = false;
-                        var surl = settings.url.replace('%QUERY', query);
+var taCol = new Bloodhound({
+    datumTokenizer: function(datum) {
+        return Bloodhound.tokenizers.whitespace(datum.value);
+    },
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    remote: {
+        wildcard: params.query,
+        url: dictUrl,
+        prepare: function(query, settings) {
+            var pq = query.split(' ');
 
-                        settings.url = surl;
-                        return settings;
-                    },
-                    transform: function (response) {
-                        console.log(JSON.stringify(response));
-                        // Map the remote source JSON array to a JavaScript object array
+            gPre = pq[pq.length-1];
+            taPick = false;
+            var surl = settings.url.replace('%QUERY',gPre);
 
-                        if (response.suggestions) {
-                            var mr = response.suggestions;
-                        } else {
-                            mr = response;
-                        }
+            settings.url = surl;
+            return settings;
+        },
+        transform: function(response) {
 
-                        var zed = $.map(mr, function (taObj) {
-                            console.log(JSON.stringify(taObj));
+            // Map the remote source JSON array to a JavaScript object array
 
-                            if (taObj.text) {
-                                return {
-                                    value: taObj.text,
-                                    magicKey: taObj.magicKey
-                                };
+            if ( response.suggestions ) {
+                var mr = response.suggestions;
+            } else  {
+                mr = response;
+            }
 
-                            } else if (typeof(taObj.concept) !== "undefined" && Array.isArray(taObj.concept.categories)) {
+            var zed = $.map(mr, function(taObj) {
 
-                                for (z = 0; z < taObj.concept.categories.length; z++) {
-                                    return {
-                                        uri: taObj.concept.uri,
-                                        categories: taObj.concept.categories[z],
-                                        value: taObj.completion
-                                    };
-                                }
+                if ( taObj.text ){
+                    return {
+                        value: taObj.text,
+                        magicKey: taObj.magicKey
+                    };
 
+                } else if ( typeof(taObj.concept) !== "undefined" && Array.isArray(taObj.concept.categories) ) {
 
-                            } else {
-                                return {
-                                    uri: taObj.concept.uri,
-                                    categories: '',
-                                    value: taObj.completion
-                                };
-                            }
-                        });
-
-                        return zed;
+                    for (z = 0; z < taObj.concept.categories.length; z++) {
+                        return {
+                            uri: taObj.concept.uri,
+                            categories: taObj.concept.categories[z],
+                            value: taObj.completion
+                        };
                     }
+
+                } else {
+                    return {
+                        uri: taObj.concept.uri,
+                        categories: '',
+                        value: taObj.completion
+                    };
                 }
             });
+
+            return zed;
+        }
+    }
+});
 
            // var taDone = function (query) {
 
@@ -74,30 +74,44 @@
                         suggestion: function (data) {
                             var conCat = '';
 
-                            if (data.categories) {
-                                conCat = '<p><strong>' + data.value + '</strong></br>' + data.categories + '</br>' + data.uri + '</p>';
-                            } else {
-                                conCat = '<p><strong>' + data.value + '</strong></p>';
-                            }
+                if ( data.categories ) {
+                    conCat = '<p><strong>' + data.value + '</strong></br>' + data.categories + '</br>' + data.uri + '</p>';
+                } else {
+                    conCat = '<p><strong>' + data.value + '</strong></p>';
+                }
 
-                            console.log('ta ' + JSON.stringify(conCat));
-                            return conCat;
-                        }
-                    }
-                }).on('typeahead:selected', function (obj, datum) {
-                    taPick = true;
-                    rexedit = datum.value;
-                    if (datum.magicKey) {
-                        gBBMagicKey = datum.magicKey
-                    }
-                });
-            };
+                return conCat;
+            }}
+    }).on('typeahead:open', function (obj, datum) {
+        console.log( ' open ');
+    }).on('typeahead:render', function (obj, datum) {
+        var fulstr = $( this ).typeahead('val');
+        console.log( ' render ' + $( this ).typeahead('val') );
 
-         taDone();
+    }).on('typeahead:active', function (obj, datum) {
+        console.log( ' active ');
+    }).on('typeahead:selected', function (obj, datum) {
+        taPick = true;
+
+        $( this ).typeahead('val', keyCol + ' ' + datum.value );
+        keyCol = $( this ).typeahead('val' );
+
+    }).focus( function () {
+        console.log('typeahead focus');
+    }).keydown( function () {
+        var key = event.keyCode || event.charCode;
+
+        if( key == 8 || key == 46 ) {
+            var etrim = $( this ).typeahead('val');
+            keyCol = etrim.slice(0, -1);;
+
+        }
+    });
 
 
+}
 
-
+taDone();
 
 
 
